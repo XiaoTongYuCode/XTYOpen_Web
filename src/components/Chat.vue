2abb546d459e4b608246dd596be20140afd685d0
Greenhorn00 <1522345857@qq.com>
2024-04-24 14:16:26 +0800

    修正

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -145,11 +145,12 @@ export default {
           let data = JSON.parse(msg.data)  // 对收到的json数据进行解析， 类似这样的： {"users": [{"username": "zhang"},{ "username": "admin"}]}
           if (data.users) {  // 获取在线人员信息
             _this.users = data.users.filter(user => user.username !== username) // 获取当前连接的所有用户信息，并且排除自身，自己不会出现在自己的聊天列表里
-          }else if(data.usersHistory){
-            _this.usersHistory = data.usersHistory.filter(user => user.username !== username)
+          }
+          if(data.usersHistory){
+            _this.usersHistory = data.usersHistory
             if (!_this.chatUser && data.users.length > 1) _this.history(data.users.filter(user => user.username !== username)[0].username);
           }
-          else {
+          if (!data.users) {
             // 如果服务器端发送过来的json数据 不包含 users 这个key，那么发送过来的就是聊天文本json数据
             //  // {"from": "zhang", "text": "hello"}
             if (data.from === _this.chatUser) {
